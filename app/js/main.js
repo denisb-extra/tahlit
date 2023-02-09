eb74c9896f7a8ea600ec225de7618f87423b4ab2
denisb-extra <45913485+denisb-extra@users.noreply.github.com>
2023-02-09 11:10:36 +0200

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function ($) {
+    window.addEventListener('scroll', onScroll);
+    function onScroll(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+            shrinkOn = 30;
+        if (distanceY > shrinkOn) {
+            $("header" ).addClass("scrolled");
+        } else {
+            $("header" ).removeClass("scrolled");
+        }
+    }
+    onScroll();
+    
+    
+    $(".mobile_menu").simpleMobileMenu({
+        "menuStyle": "slide",
+    });
+
+    $("#sm_menu_ham").on("click", function(){
+        $(".menu-top").toggleClass("open");
+    });
+
+    $("[scroll-to]").on("click", function(){
+        var trgt = $(this).attr('scroll-to');
+        $('html, body').animate({
+            scrollTop: $(trgt).offset().top
+        }, 1000);
+    });
+
+    document.addEventListener( 'wpcf7mailsent', function( event ) {
+        var inputs = event.detail.inputs;
+        thankyouPage = getFieldValueByName(inputs, "thankyou-page");
+        if(thankyouPage) window.location = thankyouPage;
+    }, false );
+});
+
+function getFieldValueByName(ar, name){
+    var result = "";
+    ar.forEach(function(item) {
+        if(item.name == name) result = item.value;
+    });
+    return result;
+}
